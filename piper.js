edeef4f11bc5f8f7d3eaf77c88c3ef4655c7aca7
Santiago Bernabé <mindcookin@gmail.com>
2015-03-19 16:53:31 +0100

    Changed chiquito to lorem-ipsum package. Added lint script.

--- piper.js
+++ piper.js
@@ -1,17 +1,20 @@
 var util = require('util'),
+    lorem = require('lorem-ipsum'),
     Transform = require('stream').Transform;
 
 function Prepend (options) {
+
   if (!(this instanceof Prepend)) {
     return new Prepend(options);
   }
 
   Transform.call(this, options);
 }
+
 util.inherits(Prepend, Transform);
 
 Prepend.prototype._transform = function (chunk, enc, cb) {
-  cb(null, "Lorem fistrum por la gloria de " + chunk.toString());
-}
+  cb(null, lorem({count: 2, units: 'words'}) + " " + chunk.toString());
+};
 
 process.stdin.pipe(Prepend()).pipe(process.stdout);
