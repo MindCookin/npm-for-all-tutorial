6f275bf4a76cd9b6ce2cca53c153bed3695a0eb6
Santiago Bernabé <mindcookin@gmail.com>
2015-03-21 21:16:24 +0100

    Watchified and npm run build

---
+++ app/widget.js
@@ -0,0 +1,18 @@
+var React = require('react'),
+    reverse = require('reverse-string'),
+    Widget;
+
+Widget = React.createClass({
+  getInitialState: function() {
+    return {message: 'Please fill input above, will be reversed :O'};
+  },
+  handleChange: function (e) {
+    var value = e.target.value ? reverse(e.target.value) : 'Please fill input above, will be reversed :O';
+    this.setState({message: value});
+  },
+  render: function () {
+    return <div><p><input onChange={this.handleChange}/></p><h4>{this.state.message}</h4></div>;
+  }
+});
+
+React.render(<Widget />, document.getElementById('container'));
